8f9a4be1ee170f251bd89da9a0a3a5d21cfdc84f
raihlima <raih.cesar@gmail.com>
2021-05-24 16:46:44 -0300

    Exercicios aula 01

---
+++ works/aula01_ex02.js
@@ -0,0 +1,73 @@
+import * as THREE from  '../build/three.module.js';
+import Stats from       '../build/jsm/libs/stats.module.js';
+import {TrackballControls} from '../build/jsm/controls/TrackballControls.js';
+import {initRenderer, 
+        initCamera,
+        InfoBox,
+        onWindowResize} from "../libs/util/util.js";
+
+var stats = new Stats();          // To show FPS information
+var scene = new THREE.Scene();    // Create main scene
+var renderer = initRenderer();    // View function in util/utils
+var camera = initCamera(new THREE.Vector3(0, -30, 15)); // Init camera in this position
+
+// Enable mouse rotation, pan, zoom etc.
+var trackballControls = new TrackballControls( camera, renderer.domElement );
+
+// Show axes (parameter is size of each axis)
+var axesHelper = new THREE.AxesHelper( 12 );
+scene.add( axesHelper );
+
+// create the ground plane
+var planeGeometry = new THREE.PlaneGeometry(20, 20);
+planeGeometry.translate(0.0, 0.0, -0.02); // To avoid conflict with the axeshelper
+var planeMaterial = new THREE.MeshBasicMaterial({
+    color: "rgba(150, 150, 150)",
+    side: THREE.DoubleSide,
+});
+var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+// add the plane to the scene
+scene.add(plane);
+
+// create a cube
+var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+var cubeMaterial = new THREE.MeshNormalMaterial();
+var cylinderGeometry = new THREE.CylinderGeometry( 1.5, 1.5, 5, 32 );
+var cylinderMaterial = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+var sphereGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+var sphereMaterial = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+var cylinder = new THREE.Mesh(cylinderGeometry, cubeMaterial);
+var sphere = new THREE.Mesh(sphereGeometry, cubeMaterial);
+
+// position the cube
+cube.position.set(0.0, 0.0, 2.0);
+cylinder.position.set(5, 0.0, 1.5);
+sphere.position.set(-5, -2.0, 1.0);
+
+// add the cube to the scene
+scene.add(cube);
+scene.add(cylinder);
+scene.add(sphere);
+
+// Use this to show information onscreen
+var controls = new InfoBox();
+  controls.add("Basic Scene");
+  controls.addParagraph();
+  controls.add("Use mouse to interact:");
+  controls.add("* Left button to rotate");
+  controls.add("* Right button to translate (pan)");
+  controls.add("* Scroll to zoom in/out.");
+  controls.show();
+
+// Listen window size changes
+window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+
+render();
+function render()
+{
+  stats.update(); // Update FPS
+  trackballControls.update(); // Enable mouse movements
+  requestAnimationFrame(render);
+  renderer.render(scene, camera) // Render scene
+}
