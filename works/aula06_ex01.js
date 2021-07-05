16f128a3667a7bbaa382ae7e5f6a91d35e9d7b15
raihlima <raih.cesar@gmail.com>
2021-07-05 11:14:22 -0300

    Aula 06 ex1 finalizado

--- works/aula06_ex01.js
+++ works/aula06_ex01.js
@@ -19,10 +19,8 @@ var renderer = initRenderer();    // View function in util/utils
   renderer.setClearColor("rgb(30, 30, 42)");
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.lookAt(0, 0, 0);
-  camera.position.set(2.18, 1.62, 3.31);
+  camera.position.set(0.0, 3.0, 6.0);
   camera.up.set( 0, 1, 0 );
-var objColor = "rgb(255,20,20)";
-var objShininess = 200;
 
 // To use the keyboard
 var keyboard = new KeyboardState();
@@ -33,7 +31,7 @@ var trackballControls = new TrackballControls( camera, renderer.domElement );
 // Listen window size changes
 window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
 
-var groundPlane = createGroundPlane(4.0, 2.5, 50, 50); // width and height
+var groundPlane = createGroundPlane(4.0, 4, 50, 50); // width and height
   groundPlane.rotateX(degreesToRadians(-90));
 scene.add(groundPlane);
 
@@ -49,123 +47,180 @@ var infoBox = new SecondaryBox("");
 
 // Teapot
 var geometry = new TeapotGeometry(0.5);
-var material = new THREE.MeshPhongMaterial({color:objColor, shininess:"200"});
+var material = new THREE.MeshPhongMaterial({color:"white", shininess:"200"});
   material.side = THREE.DoubleSide;
 var obj = new THREE.Mesh(geometry, material);
   obj.castShadow = true;
   obj.position.set(0.0, 0.5, 0.0);
 scene.add(obj);
+var animationOn = true; // Controls the teapot animation state
+var rotateTeapotClockwise = false; // Controls the teapot animation state
 
-//----------------------------------------------------------------------------
-//----------------------------------------------------------------------------
-// Control available light and set the active light
-var lightArray = new Array();
-var activeLight = 0; // View first Light
-var lightIntensity = 1.0;
+// TRACKS
+// Define materials
+var frontTrackGeometry = new THREE.BoxGeometry(4.0, 0.01, 0.01);
+var verticalTrackGeometry = new THREE.BoxGeometry(1.5, 0.01, 0.01);
+var sideTracksGeometry = new THREE.BoxGeometry(0.01, 0.01, 4.0);
+var trackMaterial = new THREE.MeshBasicMaterial( {color: "gray"} );
+// front
+var frontTrack = new THREE.Mesh(frontTrackGeometry, trackMaterial);
+frontTrack.position.set(0.0, 1.5, 2);
+scene.add(frontTrack);
 
-//---------------------------------------------------------
-// Default light position, color, ambient color and intensity
-var lightPosition = new THREE.Vector3(1.7, 0.8, 1.1);
-var lightColor = "rgb(255,255,255)";
-var ambientColor = "rgb(50,50,50)";
+// right
+var rightTrack = new THREE.Mesh(sideTracksGeometry, trackMaterial);
+rightTrack.position.set(2.0, 1.5, 0.0);
+scene.add(rightTrack);
+// left
+var leftTrack = new THREE.Mesh(sideTracksGeometry, trackMaterial);
+leftTrack.position.set(-2.0, 1.5, 0.0);
+scene.add(leftTrack);
+//vertical
+var verticalTrack1 = new THREE.Mesh(verticalTrackGeometry, trackMaterial);
+verticalTrack1.position.set(2.0, 0.75, 2);
+verticalTrack1.rotateZ(degreesToRadians(90));
+scene.add(verticalTrack1);
 
-//Luz Verde
-var greenLightPosition = new THREE.Vector3(11.7, 0.8, 1.1);
-var greenLightColor = "green";
+var verticalTrack2 = new THREE.Mesh(verticalTrackGeometry, trackMaterial);
+verticalTrack2.position.set(2.0, 0.75, -2);
+verticalTrack2.rotateZ(degreesToRadians(90));
+scene.add(verticalTrack2);
 
-//Luz Azul
+var verticalTrack3 = new THREE.Mesh(verticalTrackGeometry, trackMaterial);
+verticalTrack3.position.set(-2.0, 0.75, 2);
+verticalTrack3.rotateZ(degreesToRadians(90));
+scene.add(verticalTrack3);
+
+var verticalTrack4 = new THREE.Mesh(verticalTrackGeometry, trackMaterial);
+verticalTrack4.position.set(-2.0, 0.75, -2);
+verticalTrack4.rotateZ(degreesToRadians(90));
+scene.add(verticalTrack4);
 
 
 
-// Sphere to represent the light
-var lightSphere = createLightSphere(scene, 0.05, 10, 10, lightPosition);
-var greenLightSphere = createLightSphere(scene, 0.05, 10, 10, greenLightPosition);
-//var lightSphere = createLightSphere(scene, 0.05, 10, 10, lightPosition);
+// LIGHTS
+// Red light
+var redSpotLight = new THREE.SpotLight("red"); // red spotlight
+var redLightPosition = new THREE.Vector3(-2.0, 1.5, 0.0); // red light initial position
+//var redLightPosition = new THREE.Vector3(0.0, 1.5, 2); // red light initial position
+var redLightId = 0; // unique id
+setSpotLights(redLightPosition, redLightId);
 
-//---------------------------------------------------------
-// Create and set all lights. Only Spot and ambient will be visible at first
-var spotLight = new THREE.SpotLight(lightColor);
-setSpotLight(lightPosition);
+// Defines red light sphere
+var geometryLightsSphere = new THREE.SphereGeometry(0.05, 10, 10, 0, Math.PI * 2, 0, Math.PI);
+var materialRedLightSphere = new THREE.MeshBasicMaterial({color:"red"});
+var redLightSphere = new THREE.Mesh(geometryLightsSphere, materialRedLightSphere);
+//redLightSphere.visible = true;
+redLightSphere.position.copy(redLightPosition);
+scene.add(redLightSphere);
 
-var pointLight = new THREE.PointLight(lightColor);
-setPointLight(lightPosition);
+// Green light
+var greenSpotLight = new THREE.SpotLight("green"); // green spotlight
+var greenLightPosition = new THREE.Vector3(0.0, 1.5, 2); // green light initial position
+//var greenLightPosition = new THREE.Vector3(-2.0, 1.5, 0.0); // green light initial position
+var greenLightId = 1; // unique id
+setSpotLights(greenLightPosition, greenLightId);
 
-var dirLight = new THREE.DirectionalLight(lightColor);
-setDirectionalLighting(lightPosition);
+// Defines green light sphere
+var materialGreenLightSphere = new THREE.MeshBasicMaterial({color:"green"});
+var greenLightSphere = new THREE.Mesh(geometryLightsSphere, materialGreenLightSphere);
+//greenLightSphere.visible = true;
+greenLightSphere.position.copy(greenSpotLight);
+scene.add(greenLightSphere);
 
-// More info here: https://threejs.org/docs/#api/en/lights/AmbientLight
-var ambientLight = new THREE.AmbientLight(ambientColor);
-scene.add( ambientLight );
+// Blue light
+var blueSpotLight = new THREE.SpotLight("blue"); // blue spotlight
+var blueLightPosition = new THREE.Vector3(2.0, 1.5, 0.0); // blue light initial position
+//var blueLightPosition = new THREE.Vector3(2.0, 1.5, 0.0); // blue light initial position
+var blueLightId = 2; // unique id
+setSpotLights(blueLightPosition, blueLightId);
+
+// Defines blue light sphere
+var materialBlueLightSphere = new THREE.MeshBasicMaterial({color:"blue"});
+var blueLightSphere = new THREE.Mesh(geometryLightsSphere, materialBlueLightSphere);
+//blueLightSphere.visible = true;
+blueLightSphere.position.copy(blueSpotLight);
+scene.add(blueLightSphere);
+
 
 buildInterface();
 render();
 
-// Set Point Light
-// More info here: https://threejs.org/docs/#api/en/lights/PointLight
-function setPointLight(position)
+function setSpotLights(position, id)
 {
-  pointLight.position.copy(position);
-  pointLight.name = "Point Light"
-  pointLight.castShadow = true;
-  pointLight.visible = false;
-  spotLight.penumbra = 0.5;
-  
-  scene.add( pointLight );
-  lightArray.push( pointLight );
+  if(id == 0){ // red
+    redSpotLight.position.copy(position);
+    redSpotLight.shadow.mapSize.width = 512;
+    redSpotLight.shadow.mapSize.height = 512;
+    redSpotLight.angle = degreesToRadians(40);    
+    redSpotLight.castShadow = true;
+    redSpotLight.decay = 2;
+    redSpotLight.penumbra = 0.5;
+    redSpotLight.name = "Red Spot Light"
+
+    scene.add(redSpotLight);
+  }
+  if(id == 1){ // green
+    greenSpotLight.position.copy(position);
+    greenSpotLight.shadow.mapSize.width = 512;
+    greenSpotLight.shadow.mapSize.height = 512;
+    greenSpotLight.angle = degreesToRadians(40);    
+    greenSpotLight.castShadow = true;
+    greenSpotLight.decay = 2;
+    greenSpotLight.penumbra = 0.5;
+    greenSpotLight.name = "Green Spot Light"
+
+    scene.add(greenSpotLight);
+  }
+  if(id == 2){ // blue
+    blueSpotLight.position.copy(position);
+    blueSpotLight.shadow.mapSize.width = 512;
+    blueSpotLight.shadow.mapSize.height = 512;
+    blueSpotLight.angle = degreesToRadians(40);    
+    blueSpotLight.castShadow = true;
+    blueSpotLight.decay = 2;
+    blueSpotLight.penumbra = 0.5;
+    blueSpotLight.name = "Blue Spot Light"
+
+    scene.add(blueSpotLight);
+  }
+
 }
 
-// Set Spotlight
-// More info here: https://threejs.org/docs/#api/en/lights/SpotLight
-function setSpotLight(position)
-{
-  spotLight.position.copy(position);
-  spotLight.shadow.mapSize.width = 512;
-  spotLight.shadow.mapSize.height = 512;
-  spotLight.angle = degreesToRadians(40);    
-  spotLight.castShadow = true;
-  spotLight.decay = 2;
-  spotLight.penumbra = 0.5;
-  spotLight.name = "Spot Light"
-
-  scene.add(spotLight);
-  lightArray.push( spotLight );
+function rotateTeapot(){
+  var angle = 1; // set speed of rotation
+  if(animationOn){
+    if(rotateTeapotClockwise){
+      obj.rotateY(degreesToRadians(-angle));
+    } else {
+      obj.rotateY(degreesToRadians(+angle));
+    }
+  }
 }
 
-// Set Directional Light
-// More info here: https://threejs.org/docs/#api/en/lights/DirectionalLight
-function setDirectionalLighting(position)
+// Update light position of the spot lights
+function updateLightsPosition(id)
 {
-  dirLight.position.copy(position);
-  dirLight.shadow.mapSize.width = 512;
-  dirLight.shadow.mapSize.height = 512;
-  dirLight.castShadow = true;
+  //lightArray[activeLight].position.copy(lightPosition);
+  if (id == 0){
+    redSpotLight.position.copy(redLightPosition);
+    redLightSphere.position.copy(redLightPosition);
+    infoBox.changeMessage("Red Light Position: " + redLightPosition.x.toFixed(2) + ", " +
+                          redLightPosition.y.toFixed(2) + ", " + redLightPosition.z.toFixed(2));
+  }
+  if (id == 1){
+    greenSpotLight.position.copy(greenLightPosition);
+    greenLightSphere.position.copy(greenLightPosition);
+    infoBox.changeMessage("Green Light Position: " + greenLightPosition.x.toFixed(2) + ", " +
+                          greenLightPosition.y.toFixed(2) + ", " + greenLightPosition.z.toFixed(2));
+  }
+  if (id == 2){
+    blueSpotLight.position.copy(blueLightPosition);
+    blueLightSphere.position.copy(blueLightPosition);
+    infoBox.changeMessage("Blue Light Position: " + blueLightPosition.x.toFixed(2) + ", " +
+                          blueLightPosition.y.toFixed(2) + ", " + blueLightPosition.z.toFixed(2));
+  }
 
-  dirLight.shadow.camera.near = 1;
-  dirLight.shadow.camera.far = 20;
-  dirLight.shadow.camera.left = -5;
-  dirLight.shadow.camera.right = 5;
-  dirLight.shadow.camera.top = 5;
-  dirLight.shadow.camera.bottom = -5;
-  dirLight.name = "Direction Light";
-  dirLight.visible = false;
-
-  scene.add(dirLight);
-  lightArray.push( dirLight );
-}
-
-// Update light position of the current light
-function updateLightPosition()
-{
-  lightArray[activeLight].position.copy(lightPosition);
-  lightSphere.position.copy(lightPosition);
-  infoBox.changeMessage("Light Position: " + lightPosition.x.toFixed(2) + ", " +
-                          lightPosition.y.toFixed(2) + ", " + lightPosition.z.toFixed(2));
-}
-
-// Update light intensity of the current light
-function updateLightIntensity()
-{
-  lightArray[activeLight].intensity = lightIntensity;
 }
 
 function buildInterface()
@@ -175,130 +230,110 @@ function buildInterface()
   var controls = new function ()
   {
     this.viewAxes = false;
-    this.color = objColor;
-    this.shininess = objShininess;
-    this.lightIntensity = lightIntensity;
-    this.lightType = 'Spot'
-    this.ambientLight = true;
+    this.redSpotLight = true; // initial status
+    this.blueSpotLight = true; // initial status
+    this.greenSpotLight = true; // initial status
+    this.rotateTeapot = true; // initial status
 
-
-
-    this.onViewAxes = function(){
-      axesHelper.visible = this.viewAxes;
+    this.onEnableRedLight = function(){ // Red light power toggle
+      redSpotLight.visible = this.redSpotLight;
+      redLightSphere.visible = !redLightSphere.visible;
     };
-    this.onEnableAmbientLight = function(){
-      ambientLight.visible = this.ambientLight;
+    this.onEnableBlueLight = function(){ // Blue light power toggle
+      blueSpotLight.visible = this.blueSpotLight;
+      blueLightSphere.visible = !blueLightSphere.visible;
     };
-    this.updateColor = function(){
-      material.color.set(this.color);
+    this.onEnableGreenLight = function(){ // Green light power toggle
+      greenSpotLight.visible = this.greenSpotLight;
+      greenLightSphere.visible = !greenLightSphere.visible;
     };
-    this.onUpdateShininess = function(){
-      material.shininess = this.shininess;
+    this.onChangeAnimation = function(){ // Controls Teapot rotation
+      animationOn = !animationOn;
     };
-    this.onUpdateLightIntensity = function(){
-      lightIntensity = this.lightIntensity;
-      updateLightIntensity();
-    };
-    this.onChangeLight = function()
-    {
-      lightArray[activeLight].visible = false;
-      switch (this.lightType)
-      {
-        case 'Spot':
-            activeLight = 0;
-            break;
-        case 'Point':
-            activeLight = 1;
-            break;
-        case 'Direction':
-            activeLight = 2;
-            break;
-      }
-      lightArray[activeLight].visible = true;
-      updateLightPosition();
-      updateLightIntensity();
-    };
-
-    this.onChangeAnimation = function (){
-
-    }
   };
 
   var gui = new GUI();
-  gui.add(controls, 'viewAxes', false)
-  .name("Luz vermelha")
-  .onChange(function(e) { controls.onViewAxes() });
-  gui.add(controls, 'viewAxes', false)
-  .name("Luz verde")
-  .onChange(function(e) { controls.onViewAxes() });
-  gui.add(controls, 'viewAxes', false)
-  .name("Luz Azul")
-  .onChange(function(e) { controls.onViewAxes() });
-  gui.add(controls, 'onChangeAnimation',true).name("Animation On/Off");
-  /*
-  gui.addColor(controls, 'color')
-    .name("Obj Color")
-    .onChange(function(e) { controls.updateColor() });
-  gui.add(controls, 'shininess', 0, 1000)
-    .name("Obj Shininess")
-    .onChange(function(e) { controls.onUpdateShininess() });
-  gui.add(controls, 'viewAxes', false)
-    .name("View Axes")
-    .onChange(function(e) { controls.onViewAxes() });
-  gui.add(controls, 'lightType', ['Spot', 'Point', 'Direction'])
-    .name("Light Type")
-    .onChange(function(e) { controls.onChangeLight(); });
-  gui.add(controls, 'lightIntensity', 0, 5)
-    .name("Light Intensity")
-    .onChange(function(e) { controls.onUpdateLightIntensity() });
-  gui.add(controls, 'ambientLight', true)
-    .name("Ambient Light")
-    .onChange(function(e) { controls.onEnableAmbientLight() });
-    */
+  gui.add(controls, 'redSpotLight', true)
+    .name("Red Spot Light")
+    .onChange(function(e) { controls.onEnableRedLight() });
+  gui.add(controls, 'greenSpotLight', true)
+    .name("Green Spot Light")
+    .onChange(function(e) { controls.onEnableGreenLight() });
+  gui.add(controls, 'blueSpotLight', true)
+    .name("Blue Spot Light")
+    .onChange(function(e) { controls.onEnableBlueLight() });
+  gui.add(controls, 'onChangeAnimation',true).name("Teapot Rotation");
 }
 
 function keyboardUpdate()
 {
   keyboard.update();
-  if ( keyboard.pressed("D") )
+  if ( keyboard.pressed("D") ) // move red light to right
   {
-    lightPosition.x += 0.05;
-    updateLightPosition();
+    if (redLightPosition.z < 2){ // limit the movement only when on track
+      redLightPosition.z += 0.05;
+      updateLightsPosition(redLightId);
+    }
   }
-  if ( keyboard.pressed("A") )
+  if ( keyboard.pressed("A") ) // move red light to left
   {
-    lightPosition.x -= 0.05;
-    updateLightPosition();
+    if (redLightPosition.z > -2){ // limit the movement only when on track
+      redLightPosition.z -= 0.05;
+      updateLightsPosition(redLightId);
+    }
   }
-  if ( keyboard.pressed("W") )
+  if ( keyboard.pressed("Z") )
   {
-    lightPosition.y += 0.05;
-    updateLightPosition();
+    if (greenLightPosition.x < 2){ // limit the movement only when on track
+      greenLightPosition.x += 0.05;
+      updateLightsPosition(greenLightId);
+    }
   }
-  if ( keyboard.pressed("S") )
+  if ( keyboard.pressed("C") )
   {
-    lightPosition.y -= 0.05;
-    updateLightPosition();
+    if (greenLightPosition.x > -2){ // limit the movement only when on track
+      greenLightPosition.x -= 0.05;
+      updateLightsPosition(greenLightId);
+    }
   }
-  if ( keyboard.pressed("E") )
+  if ( keyboard.pressed("E") ) // move blue light to the front
   {
-    lightPosition.z -= 0.05;
-    updateLightPosition();
+    if (blueLightPosition.z < 2){ // limit the movement only when on track
+      blueLightPosition.z += 0.05;
+      updateLightsPosition(blueLightId);
+    }
   }
-  if ( keyboard.pressed("Q") )
+  if ( keyboard.pressed("Q") ) // move blue light backwards
   {
-    lightPosition.z += 0.05;
-    updateLightPosition();
+    if (blueLightPosition.z > -2){ // limit the movement only when on track
+      blueLightPosition.z -= 0.05;
+      updateLightsPosition(blueLightId);
+    }
   }
 }
+
+function gerarEsferas(){
+  updateLightsPosition(redLightId);
+  updateLightsPosition(greenLightId);
+  updateLightsPosition(blueLightId);
+}
+
+gerarEsferas();
 
 function showInformation()
 {
   // Use this to show information onscreen
   var controls = new InfoBox();
-    controls.add("Lighting - Types of Lights");
+    controls.add("Commands");
     controls.addParagraph();
-    controls.add("Use the WASD-QE keys to move the light");
+    controls.add("Use Q and E to move the blue light");
+    controls.add("Use A and D to move the red light");
+    controls.add("Use Z and C to move the green light");
+    controls.addParagraph();
+    controls.add("Use mouse to control camera:");
+    controls.add("* Left button to rotate");
+    controls.add("* Right button to translate (pan)");
+    controls.add("* Scroll to zoom in/out.");
     controls.show();
 }
 
@@ -308,5 +343,6 @@ function render()
   trackballControls.update();
   keyboardUpdate();
   requestAnimationFrame(render);
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
+  rotateTeapot(); // Enables teapot rotation
 }
